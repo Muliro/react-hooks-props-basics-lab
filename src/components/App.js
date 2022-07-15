02988eff519614b5cf6200c8abf6a2e5f42bfc92
michael muliro <michael.muliro@student.moringaschool.com>
2022-07-15 13:16:36 +0300

    commit tree e5979d7a5d49c8bccf5aca11453f0efcc1e0d5e7

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useReducer } from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
@@ -11,8 +11,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home  name = {user.name} color = {user.color} city = {user.city}/>
+      <About bio = {user.bio} links = {user.links}/>
     </div>
   );
 }
